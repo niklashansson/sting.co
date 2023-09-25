2e3d83cddf1351c0810f7c10bcc8d9eb0dda2f5e
niklashansson <niklas@bloomweb.co>
2023-09-25 11:30:01 +0200

    Updates

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
-import { greetUser } from '$utils/greet';
+import { swiper } from 'src/swiper2';
+
+import { annualReports } from './annualReport';
+import { jobylon } from './jobylon';
+import { library } from './library';
+import { openCoaching } from './openCoaching';
+import { senja } from './senja';
+import { globalSwiper } from './swiper';
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
-  const name = 'John Doe';
-  greetUser(name);
+  annualReports();
+  jobylon();
+  library();
+  openCoaching();
+  swiper();
+  senja();
+  // globalSwiper();
 });
